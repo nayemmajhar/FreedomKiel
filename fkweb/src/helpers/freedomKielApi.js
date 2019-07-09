cb5618d2485526d1739ff1c893feea4b9b9de1be
nayemmajhar <nayemmajhar@gmail.com>
2019-07-09 22:29:02 +0200

    Helper files for React Component added

---
+++ fkweb/src/helpers/freedomKielApi.js
@@ -0,0 +1,7 @@
+const freedomKielApi ={
+    // URL: 'http://api.freedom-kiel.com/v1',
+    URL: 'http://127.0.0.1:8080/v1',
+    siteUrl: 'http://freedom-kiel.com'
+}
+
+export default freedomKielApi;
