36cef442379295726ddbbc3b7bace3abac294a6d
nayemmajhar <nayemmajhar@gmail.com>
2019-07-09 21:55:04 +0200

    modify logo component

--- fkweb/src/components/Logo/index.js
+++ fkweb/src/components/Logo/index.js
@@ -5,8 +5,8 @@ import logo from './../../img/logo.png'
 class Logo extends React.Component{
     render(){
         return(
-            <div className="logo">
-                <NavLink to="/"><img src={logo} alt="FreedomKiel" /></NavLink>
+            <div className="d-flex align-items-center">
+                <NavLink to="/" className="navbar-brand py-1"><img className="site-logo" src={logo} alt="FreedomKiel" /></NavLink>
             </div>
         )
     }
