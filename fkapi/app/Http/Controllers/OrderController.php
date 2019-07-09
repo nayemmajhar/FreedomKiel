14f4381cdb6755e1a223891c7efa7e5283d1c70c
nayemmajhar <nayemmajhar@gmail.com>
2019-07-09 22:15:47 +0200

    order controller add and modify

---
+++ fkapi/app/Http/Controllers/OrderController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Order;
+use http\Env\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class OrderController extends Controller
+{
+
+    public function search(){
+        // return json_encode(array('name'=>'nayem','email'=>'majhar'));
+        return response()->json(Order::all());
+    }
+
+    public function showOrderById($id){
+        return response()->json(Order::find($id));
+    }
+
+    public function createOrder(Request $request)
+    {
+        // order 1 = complete, 2 = hold, -1 = failed
+        $startDate  = $request->pickup_time;
+        $endDate    = $request->dropoff_time;
+        $bike_id    = $request->bike_id;
+        $bikeIsAvailable = DB::select(DB::raw('SELECT order_items.* FROM order_items WHERE bike_id = '.$bike_id.' AND (pickup_time between \''.$startDate.'\' AND \''.$endDate.'\') AND (dropoff_time between \''.$startDate.'\' AND \''.$endDate.'\')'));
+
+        if(count($bikeIsAvailable)){
+            return response(
+                [
+                    'order'   => -1,
+                    'status'  => "success",
+                ],
+                200
+            );
+        }
+
+        $bike = DB::table('bikes')
+                    ->select(DB::raw('bikes.*'))
+                    ->where('bikes.id', '=', $bike_id)
+                    ->first();
+
+        $address_pick_drop = $bike->address .','. $bike->zip .' '. $bike->city;
+        $data = [
+            'buyer_user_id'     => $request->user_id,
+            'bike_id'           => $request->bike_id,
+            'pickup_time'       => $request->pickup_time,
+            'dropoff_time'      => $request->dropoff_time,
+            'address_pick_drop' => $address_pick_drop,
+            'rent_total'        => $request->rent_total,
+            'payment_methods'   => $request->payment_methods
+        ];
+
+        $order = Order::create($data);
+
+        $statusCode = $order ? 220:422;
+
+        return response(
+            [
+                'details'   => $order,
+                'order' => 1,
+                'status' => $order ? "success":"error"
+            ],
+            $statusCode
+        );
+
+    }
+}
